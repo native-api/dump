05dd610457428c049ad5c9c2b123ec362a273f01
Ivan Pozdeev <vano@mail.mipt.ru>
2015-02-19 20:48:58 +0300

    support strings with control characters

--- dump.py
+++ dump.py
@@ -14,7 +14,12 @@ def dump(o, methods=False):
             else: v=unicode(e)
         if (not methods) and \
             isinstance(v,(types.BuiltinFunctionType,types.FunctionType)): continue
-        v=unicode(v)
+        #escape terminal-control characters - they'd break the output
+        if isinstance(v,basestring):
+            hexpat = u'\\u%04x' if isinstance(v,unicode) else '\\x%02x'
+            v = type(v)('').join(c if ord(c)>=20 else hexpat%ord(c) for c in v)
+        try: v=unicode(v)   #for readability
+        except UnicodeDecodeError: pass     #If it fails, never mind
         limit=1024
         print f,':', v[:limit]+'...' if len(v)>limit else v
 
