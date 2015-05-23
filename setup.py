a3ef54b4dbe0fe78014b0aef29435635c2dadba4
Ivan Pozdeev <ivan_pozdeev@mail.ru>
2015-05-24 00:51:53 +0300

    +distutils installation stuff

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='dump',
+      version='1.0',
+      description='Dumping objects and other interactive console convenience functions',
+      license='MIT License',
+      py_modules=['dump'],
+      )
