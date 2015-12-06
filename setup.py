5a852403157f1711204d223c6d67d4b71b13ccf9
Ivan Pozdeev <ivan_pozdeev@mail.ru>
2015-12-06 20:24:25 +0300

    bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='dump',
-      version='1.0',
+      version='1.1',
       description='Dumping objects and other interactive console convenience functions',
       license='MIT License',
       py_modules=['dump'],
