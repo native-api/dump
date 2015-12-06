03f9fd7f747cb8f6e5d450b42bd89feb00d320a1
Ivan Pozdeev <ivan_pozdeev@mail.ru>
2015-12-06 20:23:56 +0300

    + docs

--- dump.py
+++ dump.py
@@ -3,6 +3,17 @@ import sys,os
 ironpython=hasattr(Exception,'clsException')    #the feature that interests us
 
 def dump(o, methods=False, system=False):
+    """Dump object's contents.
+
+    :param methods: include functions
+    :param system: include members with names starting and ending with double-underscore
+
+    Always skips members that yield AttrbiuteError or (in IronPython) NotSupportedException.
+    For other exceptions, prints the exception alongside the member.
+    Truncates long output (signified by ellipsis) for readability.
+    Prints national characters unless this would yield a transcoding error.
+    Non-printable characters are replaced with escape-sequences.
+    """
     import exceptions,types
     if ironpython: import System
     
@@ -28,10 +39,12 @@ def dump(o, methods=False, system=False):
         print f,':', v[:limit]+'...' if len(v)>limit else v
 
 def enum(e):
+    """Dump .NET enum members and their values."""
     import System
     t = System.Reflection.TypeDelegator(e)
     for n in t.GetEnumNames():
         print n,'=',t.GetMember(n)[0].GetRawConstantValue()
         
 def cls():
+    """clear console window and its buffer"""
     os.system("cls")
